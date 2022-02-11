19bfeec9513d5d600da0a5212bdd48a14d25faa9
justjoshriley <joshriley316@gmail.com>
2022-02-11 07:46:17 -0800

    made weatherlib

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
-// function getWeather() {
-//     // Replace this with your own API key!
-    
-//     fetch(path)
-//         .then(res => res.json())
-//         .then(json => {
-//             callback(json)
-//         })
-//         .catch(err => console.log(err.message))
-// }
-
-export async function getWeatherbyZip(apiKey, zip, unit, callback) {
-    // const units = 'imperial'
-    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`
-    const res = await  fetch(path) // waits here until the promise resolves
-    const json = await res.json() // waits here until the promise resolves
-    console.log(json)
-    const object = {
-        weather : json.weather[0].description,
-        mainTemp : json.main.temp,
-        cityName : json.name
+export class WeatherLib {
+    constructor(apiKey) {
+        this._apiKey = apiKey
     }
-    callback(object) // returns json wrapped in a Promise
-}
 
-export async function getWeatherbyCity(apiKey, city, unit, callback) {
-    // const units = 'imperial'
-    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`
-    const res = await  fetch(path) // waits here until the promise resolves
-    const json = await res.json() // waits here until the promise resolves
-    console.log(json)
-    const object = {
-        weather : json.weather[0].description,
-        mainTemp : json.main.temp,
-        cityName : json.name
+    async getWeatherByZip(zip, unit, callback) {
+        const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${this._apiKey}&units=${unit}`
+        const res = await  fetch(path)
+        const json = await res.json()
+        console.log(json)
+        const object = {
+            weather : json.weather[0].description,
+            mainTemp : json.main.temp,
+            cityName : json.name
+        }
+        callback(object)
     }
-    callback(object) // returns json wrapped in a Promise
-}
 
-export async function getWeatherbyGeo(apiKey, lat, lon, unit, callback) {
-    // const units = 'imperial'
-    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`
-    const res = await  fetch(path) // waits here until the promise resolves
-    const json = await res.json() // waits here until the promise resolves
-    console.log(json)
-    const object = {
-        weather : json.weather[0].description,
-        mainTemp : json.main.temp,
-        cityName : json.name
+    async getWeatherByCity(city, unit, callback) {
+        const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this._apiKey}&units=${unit}`
+        const res = await  fetch(path)
+        const json = await res.json()
+        const object = {
+            weather : json.weather[0].description,
+            mainTemp : json.main.temp,
+            cityName : json.name
     }
-    callback(object) // returns json wrapped in a Promise
+    callback(object)
+    }
+
+    async getWeatherbyGeo(lat, lon, unit, callback) {
+        const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this._apiKey}&units=${unit}`
+        const res = await  fetch(path)
+        const json = await res.json()
+        console.log(json)
+        const object = {
+            weather : json.weather[0].description,
+            mainTemp : json.main.temp,
+            cityName : json.name
+        }
+        callback(object)
+    }
 }
